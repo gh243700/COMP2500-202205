7b363c27ad009c53084a8020d8a328f0cb15c979
JEL97 <gh243700@gmail.com>
2022-05-02 10:54:22 +0900

    lab1_first_commit

--- Lab1/src/academy/pocu/comp2500/lab1/app/Program.java
+++ Lab1/src/academy/pocu/comp2500/lab1/app/Program.java
@@ -1,8 +1,12 @@
 package academy.pocu.comp2500.lab1.app;
 
+
 public class Program {
 
     public static void main(String[] args) {
         // Write your code here
+        int a = 5;
+        int b = 4;
+        System.out.printf("%d + %d = %d\n", a, b, Lab1.add(a, b));
     }
 }
