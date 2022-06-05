b9cb017df211069df4d16b697737cab555e733cf
JEL97 <gh243700@gmail.com>
2022-06-05 19:00:25 +0900

    Update TextAperture.java

--- Assignment2/src/academy/pocu/comp2500/assignment2/TextAperture.java
+++ Assignment2/src/academy/pocu/comp2500/assignment2/TextAperture.java
@@ -1,15 +1,13 @@
 package academy.pocu.comp2500.assignment2;
 
 public class TextAperture extends Aperture{
-    private String print;
-
-    public TextAperture(int x, int y, int length, int height, String print) {
+    private String text;
+    public TextAperture(int x, int y, int length, int height, String text) {
         super(x, y, length, height);
-        this.print = print;
+        this.text = text;
     }
-
-    public String getPrint() {
-        return print;
+    public String getText() {
+        return text;
     }
 
 }
