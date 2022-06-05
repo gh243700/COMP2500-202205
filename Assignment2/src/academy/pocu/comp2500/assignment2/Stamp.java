70faf26b6482863c71c897a5d9df00fd359f99de
JEL97 <gh243700@gmail.com>
2022-06-05 18:14:11 +0900

    Update Stamp.java

--- Assignment2/src/academy/pocu/comp2500/assignment2/Stamp.java
+++ Assignment2/src/academy/pocu/comp2500/assignment2/Stamp.java
@@ -6,6 +6,47 @@ public class Stamp extends Product{
     public String getText() {
         return text;
     }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setColor(StampType stampType) {
+        switch (stampType) {
+            case RED:
+                color = new Color(0xFF, 0x00, 0x00);
+                name = "스탬프(잉크: 빨강)";
+                break;
+            case BLUE:
+                color = new Color(0x00, 0x00, 0xFF);
+                name = "스탬프(잉크: 파랑)";
+                break;
+            case GREEN:
+                color = new Color(0x00, 0x80, 0x00);
+                name = "스탬프(잉크: 녹색)";
+                break;
+        }
+    }
+
+    public void setStampSize(StampSize stampSize) {
+        switch (stampSize) {
+            case SIZE_ONE:
+                length = 40;
+                height = 30;
+                price = 2300;
+                break;
+            case SIZE_TWO:
+                length = 50;
+                height = 20;
+                price = 2300;
+                break;
+            case SIZE_THREE:
+                length = 70;
+                height = 40;
+                price = 2600;
+                break;
+        }
+    }
     public Stamp(StampType stampType, StampSize stampSize, String text, DeliveryMethod deliveryMethod) {
         super(deliveryMethod);
         this.text = text;
