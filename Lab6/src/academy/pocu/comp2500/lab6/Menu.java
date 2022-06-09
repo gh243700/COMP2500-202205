ff22da150266b47ba7a4cb3440d147c92a51196d
JEL97 <gh243700@gmail.com>
2022-06-09 21:06:16 +0900

    lab06_change_constructor

--- Lab6/src/academy/pocu/comp2500/lab6/Menu.java
+++ Lab6/src/academy/pocu/comp2500/lab6/Menu.java
@@ -3,6 +3,10 @@ package academy.pocu.comp2500.lab6;
 public class Menu {
     protected int price;
 
+    protected Menu() {
+        
+    }
+
     public int getPrice() {
         return price;
     }
