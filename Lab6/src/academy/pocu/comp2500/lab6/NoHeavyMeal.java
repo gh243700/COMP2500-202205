4315dd9a8f34d9f1e1b5e1dcf1dcfebe70691b32
JEL97 <gh243700@gmail.com>
2022-06-09 19:33:45 +0900

    Update NoHeavyMeal.java

--- Lab6/src/academy/pocu/comp2500/lab6/NoHeavyMeal.java
+++ Lab6/src/academy/pocu/comp2500/lab6/NoHeavyMeal.java
@@ -2,21 +2,13 @@ package academy.pocu.comp2500.lab6;
 import java.util.ArrayList;
 public class NoHeavyMeal extends Meal {
     private static final int PRICE = 15;
-
     private ArrayList<Appetizer> appetizers = new ArrayList<>();
-
     public NoHeavyMeal() {
         price = PRICE;
     }
-
     public boolean isValid() {
         return this.appetizers.size() == 2 && this.desserts.size() != 0;
     }
-
-    public ArrayList<Appetizer> getAppetizers() {
-        return this.appetizers;
-    }
-
     public void setAppetizers(Appetizer appetizer1, Appetizer appetizer2) {
         this.appetizers.clear();
 
