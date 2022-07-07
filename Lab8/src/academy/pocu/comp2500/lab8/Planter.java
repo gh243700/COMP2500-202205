adf3793250b2ba31e4918ae3435d2a9bc8b0f157
JEL97 <gh243700@gmail.com>
2022-07-07 17:53:07 +0900

    Update Planter.java

--- Lab8/src/academy/pocu/comp2500/lab8/Planter.java
+++ Lab8/src/academy/pocu/comp2500/lab8/Planter.java
@@ -40,6 +40,9 @@ public class Planter {
     }
 
     public void tick() {
+        for (IWaterDetectable iWaterDetectable : waterDetectables) {
+            iWaterDetectable.detect(waterAmount);
+        }
 
         for (SmartDevice smartDevice : smartDevices) {
             smartDevice.onTick();
@@ -54,11 +57,6 @@ public class Planter {
         }
 
         addWaterAmount(WATER_CONSUME_PER_DAY);
-
-        for (IWaterDetectable iWaterDetectable : waterDetectables) {
-            iWaterDetectable.detect(waterAmount);
-        }
-
     }
 
 }
