1968d664466d0e80b680c2cce2be977eb86ecc39
JEL97 <gh243700@gmail.com>
2022-07-11 20:54:58 +0900

    Update Cart.java

--- Lab9/src/academy/pocu/comp2500/lab9/Cart.java
+++ Lab9/src/academy/pocu/comp2500/lab9/Cart.java
@@ -2,6 +2,7 @@ package academy.pocu.comp2500.lab9;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.UUID;
 
 public final class Cart {
     private ArrayList<Book> books = new ArrayList<>();
@@ -18,14 +19,21 @@ public final class Cart {
         return this.books.size();
     }
 
-    public void addBooks(Collection<Book> books) {
-        this.books.addAll(books);
+    public void addBooks(final UUID[] skus, final String[] titles, final int[] prices, final int[] publishedYears) {
+        if (skus.length != titles.length || skus.length != prices.length || skus.length != publishedYears.length) {
+            return;
+        }
+
+        for (int i = 0; i < skus.length; ++i) {
+            Book book = new Book(skus[i], titles[i], prices[i], publishedYears[i]);
+            this.books.add(book);
+        }
     }
 
-    public void addBook(Book book) {
+    public void addBook(final UUID sku, final String title, final int price, final int publishedYear) {
+        Book book = new Book(sku, title, price, publishedYear);
         this.books.add(book);
     }
-
     public boolean remove(final int index) {
         if (this.books.size() <= index) {
             return false;
