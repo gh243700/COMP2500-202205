2518b5fb5fffa946e33c529f440d6e1162bb2dda
JEL97 <gh243700@gmail.com>
2022-07-21 20:02:22 +0900

    Update MaintenanceMiddleware.java

--- Lab10/src/academy/pocu/comp2500/lab10/MaintenanceMiddleware.java
+++ Lab10/src/academy/pocu/comp2500/lab10/MaintenanceMiddleware.java
@@ -7,7 +7,6 @@ import java.time.OffsetDateTime;
 public class MaintenanceMiddleware implements IRequestHandler {
     private IRequestHandler iRequestHandler;
     private OffsetDateTime maintenanceTime;
-
     public MaintenanceMiddleware(IRequestHandler iRequestHandler, OffsetDateTime maintenanceTime) {
         this.iRequestHandler = iRequestHandler;
         this.maintenanceTime = maintenanceTime;
@@ -16,24 +15,9 @@ public class MaintenanceMiddleware implements IRequestHandler {
     @Override
     public ResultBase handle(Request request) {
         OffsetDateTime now = OffsetDateTime.now();
-        int t = now.getHour() * 60 + now.getMinute() - maintenanceTime.getHour() * 60 - maintenanceTime.getMinute();
-        if (!now.isBefore(maintenanceTime) && now.getYear() == maintenanceTime.getYear()
-                && now.getMonth() == maintenanceTime.getMonth()
-                && now.getDayOfMonth() == maintenanceTime.getDayOfMonth()
-                && t > 60) {
-            t = maintenanceTime.getHour() * 60 + 60 + maintenanceTime.getMinute();
-            int hour = t / 60;
-            int min = t % 60;
-
-            return new ServiceUnavailableResult(maintenanceTime,
-                    OffsetDateTime.of(
-                    maintenanceTime.getYear(),
-                    maintenanceTime.getMonthValue(),
-                    maintenanceTime.getDayOfMonth(),
-                    hour, min, maintenanceTime.getSecond(),
-                    maintenanceTime.getNano(),
-                    maintenanceTime.getOffset())
-            );
+        OffsetDateTime end = maintenanceTime.plusHours(1);
+        if (now.isAfter(maintenanceTime) && now.isBefore(end)) {
+            return new ServiceUnavailableResult(maintenanceTime, end);
         }
 
         return iRequestHandler.handle(request);
