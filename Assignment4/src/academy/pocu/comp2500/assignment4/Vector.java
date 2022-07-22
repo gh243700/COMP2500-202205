b780c2f9d86d0b8f6d9936abca2723f94b087b1e
JEL97 <gh243700@gmail.com>
2022-07-22 17:23:14 +0900

    Update Vector.java

--- Assignment4/src/academy/pocu/comp2500/assignment4/Vector.java
+++ Assignment4/src/academy/pocu/comp2500/assignment4/Vector.java
@@ -3,20 +3,10 @@ package academy.pocu.comp2500.assignment4;
 public class Vector {
     private int x;
     private int y;
-
     public Vector(int x, int y) {
         this.x = x;
         this.y = y;
     }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
     @Override
     public boolean equals(Object obj) {
         if (obj == null || obj instanceof Vector == false) {
