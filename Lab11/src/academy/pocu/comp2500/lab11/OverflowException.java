9140fb9f7bb2590480fe252b9c8475c408ececab
JEL97 <gh243700@gmail.com>
2022-07-26 21:46:17 +0900

    Update OverflowException.java

--- Lab11/src/academy/pocu/comp2500/lab11/OverflowException.java
+++ Lab11/src/academy/pocu/comp2500/lab11/OverflowException.java
@@ -1,14 +1,12 @@
 package academy.pocu.comp2500.lab11;
 
 public class OverflowException extends RuntimeException {
-    private String message;
-
     @Override
     public String getMessage() {
-        return message;
+        return super.getMessage();
     }
 
     public OverflowException(String message) {
-        this.message = message;
+        super(message);
     }
 }
